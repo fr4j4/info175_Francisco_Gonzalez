e616d22ae61e24482723c5130c81eda6b377059a
fr4j4 <fgonz@outlook.com>
2016-03-24 12:27:13 -0300

    ahora el metodo encrypt acepta frases mayusculas o minusculas

--- Ejercicios_clase2_24-03-2016/Ejer2.py
+++ Ejercicios_clase2_24-03-2016/Ejer2.py
@@ -3,18 +3,24 @@ import string
 def encrypt(palabra,n):
 	p=""
 	letra=False
-	abc=string.ascii_lowercase
+	abc_l=string.ascii_lowercase
+	abc_u=string.ascii_uppercase
 	for i in range (0,len(palabra)):
 		letra=False
-		for j in range (0,len(abc)):
-			if(palabra[i]==abc[j]):
-				if(j+n>len(abc)-1):
-					n=n-len(abc)
-				p=p+abc[j+n]
+		for j in range (0,len(abc_l)):
+			if(palabra[i]==abc_l[j]):
+				if(j+n>len(abc_l)-1):
+					n=n-len(abc_l)
+				p=p+abc_l[j+n]
+				letra=True;
+			elif(palabra[i]==abc_u[j]):
+				if(j+n>len(abc_u)-1):
+					n=n-len(abc_u)
+				p=p+abc_u[j+n]
 				letra=True;
 		if letra==False:
 			p=p+palabra[i]
 	return p
 
 if __name__=="__main__":
-	print(encrypt(raw_input("Ingrese una palabra: ").lower(),int(raw_input("Ingrese un numero: "))))
+	print(encrypt(raw_input("Ingrese una palabra: "),int(raw_input("Ingrese un numero: "))))
