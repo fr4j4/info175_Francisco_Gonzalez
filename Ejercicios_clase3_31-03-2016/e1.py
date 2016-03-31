256f014a5f4bfdafb554e70fabb24e90c8b43a93
fr4j4 <fgonz@outlook.com>
2016-03-31 11:53:20 -0300

    Agregado ejercicio 1 - clase 3

---
+++ Ejercicios_clase3_31-03-2016/e1.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+class Auto(object):
+	def __init__(self):
+		self.kilometraje=0
+		self.bencina=0
+		self.rendimiento=10
+		self.encendido=False
+	def encender(self):
+		print "bum bum bummmm (encendido)"
+		self.encendido=True;
+	def apagar(self):
+		print "ZzZzZzZzZz (apagado)"
+		self.encendido=True;
+	def mover (self,distancia):
+		if self.encendido:
+			if self.rendimiento*self.bencina>=distancia:
+				print "moviendo...\n...Se ha desplazado "+str(distancia)+" km"
+			else:
+				print "falta bencina para mover"
+		else:
+			print "El auto está apagado, no se puede mover"
+		
+	def get_kilometraje(self):
+		return self.kilometraje
+	def get_bencina(self):
+		return self.bencina
+	def cargar_bencina(self,b):
+		if self.encendido:
+			print ("se ha cargado "+str(b)+" lt. de bencina")
+			self.bencina+=b
+		else:
+			print "Debe apagar el auto antes de cargar"
+
+a1=Auto()
+a1.encender()
+a1.apagar()
+a1.cargar_bencina(40)
+a1.encender()
+a1.mover(50)
