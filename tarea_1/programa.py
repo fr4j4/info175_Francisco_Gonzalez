a61fbdaf962f42cd52c61e8d2152b3bd7bf99f5a
fr4j4 <fgonz@outlook.com>
2016-03-31 02:38:34 -0300

    Tarea terminada!

--- tarea_1/programa.py
+++ tarea_1/programa.py
@@ -11,7 +11,7 @@ def encrypt(text):
 	if(var_metodo.get()==2):
 		out=cenitpolar(text)
 	elif(var_metodo.get()==1):
-		out=cesar(text,salto)
+		out=cesar(text,salto.get())
 	return out
 def settext(text):
 	texto_salida.config(state="normal")
@@ -94,18 +94,20 @@ def cesar(palabra,n):
 root =Tk()
 
 var_metodo=IntVar()
+salto=IntVar()
 
 root.geometry("320x320")#tamaño ventana
 root.title("Encriptador super-secreto")#titulo ventana
 
+#definicionn de los widgets que componen la ventana
 label_1=Label(root,text="Ingrese texto_entrada a encriptar")
 label_2=Label(root,text="Seleccione un método de encriptacion")
 label_3=Label(root,text="Especifique un salto\npara la encriptaicon cesar")
 label_4=Label(root,text="Texto encriptado")
 
-empty_label_1=Label(root,text="")
+empty_label_1=Label(root,text="")#label vacio, para hacer un "salto de fila"
 
-spin=Spinbox(root,width=2,from_=0, to=sys.maxsize,state="readonly")
+spin=Spinbox(root,width=2,from_=0, to=sys.maxsize,state="readonly",textvariable=salto)
 
 texto_entrada=Text(root,width="45",height="5")
 texto_salida=Text(root,width="45",height="5",state="disabled")
@@ -116,6 +118,7 @@ radio_1=Radiobutton(root,text="césar",variable=var_metodo,value=1,command=lambd
 radio_2=Radiobutton(root,text="cenit-polar",variable=var_metodo,value=2,command=lambda:spin.config(state="disabled"))#value=2
 
 
+#definir posiciones de los widgets
 label_2.grid(row=2,columnspan=2)
 label_1.grid(row = 0,columnspan=2)
 texto_entrada.grid(row = 1,column=0,columnspan=2)
